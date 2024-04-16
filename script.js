2a49cee36ce2037012108f0c1375a235aea70d0e
hamuzah <37175583+hamuzah@users.noreply.github.com>
2024-04-16 13:41:33 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
 // your code here
+    if(num<=0){
+		return -1;
+	}
+	if (num == 1) {
+		return 0;
+	}
+
+	let fibprev = 0;
+	let fibcurr = 1;
+
+	for(let i = 2; i<num; i++){
+		let temp = fibcurr;
+		fibcurr = fibcurr + fibprev;
+		fibprev = temp;
+	}
+	return fibcurr;
 }
 
 module.exports = fibonacci;
